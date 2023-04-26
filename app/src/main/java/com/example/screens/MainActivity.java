d026189998cd1972b3729f315ae9d5923d75ba18
Axel <a22axeju@student.his.se>
2023-04-26 12:54:40 +0200

    Added a startSecondActivity() method that reads data from EditText and add it to the intent.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,9 +2,22 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void startSecondActivity(View view) {
+        EditText nameEditText = findViewById(R.id.name_edittext);
+        String name = nameEditText.getText().toString();
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("name", name);
+        startActivity(intent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
